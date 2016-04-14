a0aa6c4c93279e58576657c4bbc0ebae92756b1a
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-04-14 17:53:34 +0700

    Commit

--- kalender_tanam_padi.php
+++ kalender_tanam_padi.php
@@ -1,5 +1,8 @@
 <?php
 
+require 'HijriDateTime.php';
+$hdate = new HijriDateTime();
+
 $mulai_tanam = '2016-01-19';
 
 $tanggal_pupuk = array(-1, 14, 30, 45);
@@ -19,7 +22,7 @@ for ($i = -1; $i <= 90; $i++) {
     $tanggal = strtotime($i . ' day', $tanggal_mulai_tanam);
     if (in_array($i, $tanggal_pupuk)) {
         echo "<tr>";
-        echo "<td>" . date('d F Y', $tanggal) . "</td><td>" . "Tanggal Pupuk H" . ($i < 0 ? $i : "+" . $i) . "</td>";
+        echo "<td>" . date('d F Y', $tanggal) . "</td><td>" . "Semprot Pupuk Cair Organik H" . ($i < 0 ? $i : "+" . $i) . "</td>";
         echo "</tr>";
     } else 
     if ($i == 0) {
@@ -33,31 +36,20 @@ for ($i = -1; $i <= 90; $i++) {
         echo "</tr>";
     }
     
-    $result = file_get_contents("http://www.moonsighting.org.uk/scripts/hijri.js?return=json&date=".date('Y-m-d', $tanggal));
-    $json = json_decode($result);
-    $int = filter_var($json->islamic[1], FILTER_SANITIZE_NUMBER_INT);
+//    $result = file_get_contents("http://www.moonsighting.org.uk/scripts/hijri.js?return=json&date=".date('Y-m-d', $tanggal));
+//    $json = json_decode($result);
+//    $int = filter_var($json->islamic[1], FILTER_SANITIZE_NUMBER_INT);
 //    var_dump($json->islamic);
 //    var_dump($int);
 //    break;
-    if ($int == 13) {
+    
+    $hijri = $hdate->GeToHijr(date('d', $tanggal), date('m', $tanggal), date('Y', $tanggal));
+//    var_dump($hijri);
+            
+    if ($hijri['day'] == 13) {
         echo "<tr>";
-        echo "<td>" . date('d F Y', $tanggal) . "</td><td>" . "Tanggal Semprot Pestisida (13 Bulan Hijriah)" . "</td>";
+        echo "<td>" . date('d F Y', $tanggal) . "</td><td>" . "Semprot Pestisida H" . ($i < 0 ? $i : "+" . $i) . " (13 Bulan Hijriah)" . "</td>";
         echo "</tr>";
     }
 }
-//echo "<tr>";
-//echo "<td>" . date('d F Y', $tanggal_pupuk_H_min1) . "</td><td>" . "Tanggal Pupuk H-1" . "</td>";
-//echo "</tr>";
-//echo "<tr>";
-//echo "<td>" . date('d F Y', $tanggal_pupuk_H_14) . "</td><td>" . "Tanggal Pupuk H+14" . "</td>";
-//echo "</tr>";
-//echo "<tr>";
-//echo "<td>" . date('d F Y', $tanggal_pupuk_H_30) . "</td><td>" . "Tanggal Pupuk H+30" . "</td>";
-//echo "</tr>";
-//echo "<tr>";
-//echo "<td>" . date('d F Y', $tanggal_pupuk_H_45) . "</td><td>" . "Tanggal Pupuk H+45" . "</td>";
-//echo "</tr>";
-//echo "<tr>";
-//echo "<td>" . date('d F Y', $tanggal_panen) . "</td><td>" . "Tanggal Panen H+90" . "</td>";
-//echo "</tr>";
 echo "</table>";
